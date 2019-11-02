47158a2fefea1e13b942056bfbdd7b133d04ffa5
Lv Xinpeng <48200531+lvxinpeng1@users.noreply.github.com>
2019-11-02 20:01:15 +0800

    Add files via upload

---
+++ homework3/Lv_Xinpeng/model.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  1 19:51:23 2019
+
+@author: LVXINPENG
+"""
+
+import torch
+import torch.nn as nn
+
+# 循环神经网络 (many-to-one)
+class TextRNN(nn.Module):
+    def __init__(self, args):
+        """
+        Q1:
+            Please define your model here.
+            args is a bunch of parameters, 
+            you can view them in train.py ,
+            and use it just like ' self.hidden_size = args.hidden_size' .
+        """
+        
+        
+       
+    def forward(self, x):
+        """
+        Q2:
+            Please define the forword propagation 
+            you can view the dataset(tsv) for obtaining the dimension of data
+            and make sure that 'out = '
+        """
+        
+        return out
+
