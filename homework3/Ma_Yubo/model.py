9b3cb15e69a1dfd4244ac240105c07a90756a195
MaYB <mayb15@mails.tsinghua.edu.cn>
2019-11-10 22:56:19 +0800

    uplaoded

---
+++ homework3/Ma_Yubo/model.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  1 19:51:23 2019
+
+@author: LVXINPENG
+"""
+
+import torch
+import torch.nn as nn
+
+class TextRNN(nn.Module):
+    def __init__(self, args):
+        """
+        Q3:
+            Please define your model here.
+            args is a bunch of parameters, 
+            you can view them in train.py ,
+            and use it just like ' self.hidden_size = args.hidden_size' .
+        """
+
+        super(TextRNN, self).__init__()
+        self.embedding_dim = args.embedding_dim
+        self.hidden_size = args.hidden_size
+        self.vocab_size = args.vocab_size
+        self.layer_num = args.layer_num
+        self.label_num = args.label_num
+        self.bidirectional = args.bidirectional
+
+        self.embed = nn.Embedding(self.vocab_size, self.embedding_dim)
+        self.lstm = nn.LSTM(self.embedding_dim, self.hidden_size, self.layer_num,
+                            batch_first=True, bidirectional=self.bidirectional)
+        if self.bidirectional:
+            self.linear = nn.Linear(self.hidden_size*2, self.label_num)
+        else:
+            self.linear = nn.Linear(self.hidden_size, self.label_num)
+        
+       
+    def forward(self, x):
+        """
+        Q4:
+            Please define the forword propagation 
+            you can view the dataset(tsv) for obtaining the dimension of data
+            and make sure that 'out = '
+        """
+        embed = self.embed(x)
+        out, (hidden, cell) = self.lstm(embed)
+        out = self.linear(out[:, -1, :])
+        return out
+
